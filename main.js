1310baaee01f7e996379501deed23ddcc54f8ad4
Kdampil <wallycrism@gmail.com>
2025-02-08 04:24:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 768,
+        frame: false, // Borderless window
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    mainWindow.loadFile('index.html');
+    mainWindow.on('closed', () => (mainWindow = null));
+}
+
+app.whenReady().then(createWindow);
+
+// Handle "Always on Top" toggle
+ipcMain.on('set-always-on-top', (event, isChecked) => {
+    if (mainWindow) {
+        mainWindow.setAlwaysOnTop(isChecked);
+    }
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
